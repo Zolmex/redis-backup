6b3131db6caf675a01f7e3e7934bbefa35c9ae58
Zolmex <zolmexyt@gmail.com>
2020-11-03 13:57:57 -0300

    Project files

---
+++ Redis Backup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Redis_Backup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
